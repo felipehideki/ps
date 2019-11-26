943f6c29ef9d86eb249d518d84887057ff2d1843
felipehideki <feehide@gmail.com>
2019-11-26 00:15:12 -0300

    Update projetoSP.m

--- PROJETO/projetoSP.m
+++ PROJETO/projetoSP.m
@@ -15,16 +15,15 @@ fs = 44100; % Frequência de amostragem dada pelo enunciado
 ttotal = numel(sinal_final)/fs; % Tamanho da gravação em segundos
 t = 1/fs:1/fs:ttotal;
 figure(1);
-hold on;
 plot(t,sinal_final);
+hold on;
 
 % % Espectro da energia
 sigfft = fftshift(fft(sinal_final));
 freqnorm = linspace(-1,1,numel(sigfft));
 figure(2);
-hold on;
 plot(freqnorm,2*(abs(sigfft).^2),'LineWidth',1.0); % Espectro de energia
-xlim([0 1]);
+hold on;
 
 % % Filtro FIR para frequência normalizada (wc 1633Hz ~ wcn 0.08)
 filtroAA = fir1(100,0.08,'low'); % Janelamento Hamming
@@ -51,18 +50,12 @@ fatorDS = floor(fs/(2*1633));
 sinalDS = downsample(sinal_comAA,fatorDS);
 fsdown = fs/fatorDS;
 tDS = (0:1/(fs/fatorDS):ttotal);
-% figure(4);
-% plot(tDS,sinalDS,'LineWidth',1.0);
-% title('Sinal após filtragem AA e downsampling');
-% xlabel('Segundos');
-
 sinalDSfft = fftshift(fft(sinalDS));
 freqnormDS = linspace(-1,1,numel(sinalDSfft));
 figure(3);
-hold on;
 plot(freqnormDS,2*(abs(sinalDSfft).^2),'LineWidth',1.0);
-xlim([0 1]);
 title('Espectro da energia do sinal downsampled');
+xlim([0 1]);
 xlabel('Frequência normalizada');
 
 %% b) Projetando FIR para remover frequências indesejadas
@@ -72,20 +65,38 @@ xlabel('Frequência normalizada');
 % % frequências é um método eficiente para remover a maior parte das
 % % frequências indesejadas.
 % % Em range normalizado, 697Hz corresponde a 697/(fsdown/2) = 0.1027. 
-% % Portanto, uma frequência de corte de 0.1 para a filtragem
-% % passa-alta, garantindo que 697Hz esteja seguramente dentro da banda.
-% % Da mesma forma, 1633Hz corresponde a aproximadamente 0.9628, então
-% % foi utilizada uma frequência de corte de 0.97 para a filtragem
-% % passa-baixa.
+% % Da mesma forma, 1633Hz corresponde a aproximadamente 0.9628.
+% % Para realizar uma atenuação de 99.999% das frequências indesejadas, é
+% % necessário ajustar a frequência de corte para os filtros projetados tal
+% % que o decaimento seja 20log(0.00001) = -120dB.
 
-wn = [0.1 0.97];
-filtroHamming = fir1(1000,wn,rectwin(1001));
+wn = [0.3 0.9];
+
+% Hamming
+filtroHamming = fir1(1000,wn,hamming(1001));
 sigHamming = conv(sinalDS,filtroHamming,'same');
 sigHammingfft = fftshift(fft(sigHamming));
-figure(3);
+figure(4);
+subplot(2,1,1);
+plot(freqnormDS,2*(abs(sinalDSfft).^2),'LineWidth',1.0);
+title('Espectro da energia do sinal downsampled vs. filtrado');
+hold on;
 plot(freqnormDS,2*(abs(sigHammingfft).^2),'LineWidth',1.0);
 xlim([0 1]);
-title('Espectro da energia do sinal downsampled filtrado');
 xlabel('Frequência normalizada');
-legend('Downsampled s/filtro','c/filtro');
+legend('Downsampled s/filtro','Hamming');
+% freqz(filtroHamming);
 
+% Kaiser
+filtroKaiser = fir1(1000,wn,kaiser(1001,4));
+sigKaiser = conv(sinalDS,filtroKaiser,'same');
+sigKaiserfft = fftshift(fft(sigKaiser));
+figure(4);
+subplot(2,1,2);
+plot(freqnormDS,2*(abs(sinalDSfft).^2),'LineWidth',1.0);
+hold on;
+plot(freqnormDS,2*(abs(sigKaiserfft).^2),'LineWidth',1.0);
+xlim([0 1]);
+xlabel('Frequência normalizada');
+legend('Downsampled s/filtro','Kaiser');
+% freqz(filtroKaiser);
